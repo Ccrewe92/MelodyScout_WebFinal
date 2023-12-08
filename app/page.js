7409d30af6d889af091a9b6ec931f48f4ea619af
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-12-08 14:44:17 -0700

    navbar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar /> {/* Include the Navbar component here */}
       <h1>Welcome to MelodyScout</h1>
       <p>Discover and enjoy personalized music recommendations.</p>
       {/* You can add more content here */}
