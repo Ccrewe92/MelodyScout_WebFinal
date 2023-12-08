78a5d6ce804882acb924905f79d2a7422df61dcd
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-12-08 14:24:21 -0700

    new melodyscout

---
+++ app/utils/spotify.js
@@ -0,0 +1,6 @@
+const spotifyConfig = {
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+};
+
+export default spotifyConfig;
---
+++ app/utils/spotifyapi.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import qs from "qs";
+import spotifyConfig from "../spotify";
+
+const BASE_URL = "https://accounts.spotify.com/api";
+const SPOTIFY_API_URL = "https://api.spotify.com/v1";
+
+export const getAccessToken = async () => {
+  const credentials = `${spotifyConfig.clientId}:${spotifyConfig.clientSecret}`;
+  const encodedCredentials = Buffer.from(credentials).toString("base64");
+
+  const tokenResponse = await axios.post(
+    `${BASE_URL}/token`,
+    qs.stringify({ grant_type: "client_credentials" }),
+    {
+      headers: {
+        Authorization: `Basic ${encodedCredentials}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+
+  return tokenResponse.data.access_token;
+};
+
+export const searchTracks = async (accessToken, query) => {
+  const response = await axios.get(`${SPOTIFY_API_URL}/search`, {
+    params: {
+      q: query,
+      type: "track",
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return response.data.tracks.items;
+};
