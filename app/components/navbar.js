00587bed5a4765a2873a6d4f9d4643fc9f0eab41
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-12-08 17:18:49 -0700

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -16,11 +16,7 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/">
           <a className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img
-              src="../shared/melodyscout.png"
-              className="h-8"
-              alt="Alt Logo"
-            />
+            <img src="../shared/melodyscout.png" className="h-8" alt="Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Flowbite
             </span>
