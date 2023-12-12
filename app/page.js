efeabae370c7246f4e461bb6075c6cea092c500f
DevonWoodruff <119452528+DevonWoodruff@users.noreply.github.com>
2023-12-11 22:01:47 -0700

    Can't get log out to work

--- app/page.js
+++ app/page.js
@@ -7,14 +7,13 @@ import qs from "qs";
 import { searchTracks } from "./utils/spotifyapi";
 import { useSearchParams } from "next/navigation";
 import spotifyConfig from "./utils/spotify";
-import Cookies from "js-cookie";
 
 export default function Home() {
   const [authToken, setAuthToken] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
   const [recommendations, setRecommendations] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [code, setCode] = useState(null);
+  const [code, setCode] = useState(null); // State for the code
 
   const params = useSearchParams();
 
@@ -28,13 +27,12 @@ export default function Home() {
         body: JSON.stringify({ code }),
       });
       const data = await response.json();
-      setAuthToken(data.access_token);
-      Cookies.set("userToken", data.access_token); // Store token in cookie
       return data.access_token;
     } catch (error) {
       console.error("Error fetching access token:", error);
     }
   };
+
   useEffect(() => {
     const fetchedCode = params.get("code");
     if (fetchedCode) {
@@ -75,44 +73,46 @@ export default function Home() {
     }
   };
 
+  const loginUrl =
+    "https://accounts.spotify.com/authorize?" +
+    qs.stringify({
+      response_type: "code",
+      client_id: spotifyConfig.clientId,
+      scope: "user-read-private user-read-email",
+      redirect_uri: spotifyConfig.redirectUri,
+      state: "12321",
+    });
+
   return (
-    <div className="flex flex-col min-h-screen bg-black text-white">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       {/* Search Bar */}
-      <div className="flex justify-center mt-10">
-        <form onSubmit={handleSearch} className="flex w-full max-w-xl">
+      <form onSubmit={handleSearch}>
+        <div className="search-bar-container">
           <input
             type="text"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="Search for tracks"
-            className="flex-1 p-2 border-2 border-r-0 border-green-500 focus:outline-none bg-gray-800 text-white"
           />
-          <button
-            type="submit"
-            className="bg-green-500 px-4 py-2 border-2 border-green-500 hover:bg-green-600 transition-colors"
-          >
-            Search
-          </button>
-        </form>
-      </div>
+          <button type="submit">Search</button>
+        </div>
+      </form>
       {/* Recommendations Container */}
-      <div className="flex flex-wrap justify-center gap-4 m-4">
+      <div className="recommendations-container">
         {recommendations.map((track) => (
-          <div key={track.id} className="song-card max-w-sm bg-gray-800 shadow-lg rounded-lg overflow-hidden">
-            <div className="px-6 py-4">
-              <h3 className="font-bold text-xl mb-2">{track.name}</h3>
-              <p className="text-gray-400 text-base">
-                Artist: {track.artists.map((artist) => artist.name).join(", ")}
-              </p>
-            </div>
+          <div key={track.id} className="song-card">
+            <h3>{track.name}</h3>
+            <p>
+              Artist: {track.artists.map((artist) => artist.name).join(", ")}
+            </p>
           </div>
         ))}
       </div>
       {/* Main Content */}
-      <main className="flex-grow flex items-center justify-center flex-col">
-        <h1 className="text-4xl font-bold">Welcome to MelodyScout</h1>
-        <p className="text-xl mt-2">Discover and enjoy personalized music recommendations.</p>
+      <main className="flex-grow">
+        <h1>Welcome to MelodyScout</h1>
+        <p>Discover and enjoy personalized music recommendations.</p>
         {/* Logout Button */}
       </main>
       <Footer />
