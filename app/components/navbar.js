4875c6afd1154e5b6891f4a9da0eb193a68a5c3f
Ojifu <ahmedhijazi49@gmail.com>
2023-12-11 17:08:40 -0700

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,79 +1,65 @@
-"use client";
-import Link from "next/link";
+// Navbar.js
 import React, { useState, useEffect } from "react";
-import qs from "qs";
 import Cookies from "js-cookie";
-import spotifyConfig from "../utils/spotify";
+import Link from "next/link";
+import qs from "qs";
+import spotifyConfig from "../utils/spotify"; // Make sure this path is correct
 
 const Navbar = () => {
-  // State to track the login status and user token
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userToken, setUserToken] = useState(null);
+  // State to manage login status based on the userToken cookie
+  const [isLoggedIn, setIsLoggedIn] = useState(Cookies.get("userToken") ? true : false);
 
-  // Handler function for the "Log In" button click
+  // Effect to set the isLoggedIn state based on the cookie presence
+  useEffect(() => {
+    const token = Cookies.get("userToken");
+    setIsLoggedIn(!!token); // The double bang (!!) converts a truthy/falsy value to a boolean true/false
+  }, []);
+
+  // Handler for login button click
   const handleLoginClick = () => {
-    console.log("Log In button clicked");
-    // Define the Spotify API endpoint
+    // Define Spotify's authorization endpoint
     const spotifyAuthEndpoint = "https://accounts.spotify.com/authorize";
-
-    // Specify the scopes (permissions) your app needs
+    // Define the scopes required for the Spotify access
     const scopes = ["user-read-private", "user-read-email"];
-
-    // Construct the authorization URL with query parameters
-    const authUrl =
-      `${spotifyAuthEndpoint}?` +
-      qs.stringify({
-        client_id: "74f2f2fb31124bae932f4c83f5f3b337",
-        redirect_uri: "https://melody-scout-web-final.vercel.app/", // Replace with your actual redirect URI
-        response_type: "code",
-        scope: scopes.join(" "),
-      });
-
-    // Redirect the user to the Spotify authorization page
+    // Construct the full authorization URL
+    const authUrl = `${spotifyAuthEndpoint}?${qs.stringify({
+      client_id: spotifyConfig.clientId,
+      redirect_uri: spotifyConfig.redirectUri,
+      response_type: "code",
+      scope: scopes.join(" "),
+      state: spotifyConfig.state, // Ensure you have a state value defined in spotifyConfig
+    })}`;
+    // Redirect the user to Spotify's authorization page
     window.location.href = authUrl;
   };
 
-  // Handler function for the "Log Out" button click
+  // Handler for logout button click
   const handleLogoutClick = () => {
-    // Clear the user token from cookies
-    Cookies.remove("userToken");
-    setUserToken(null);
-
-    // Add your additional logout logic here
-    // For example, you can perform any necessary cleanup
-    console.log("Log Out button clicked");
-    setIsLoggedIn(false);
-  };
-
-  useEffect(() => {
-    // Check if the user is already logged in (you can replace this with your actual check)
-    const storedUserToken = Cookies.get("userToken");
-
-    if (storedUserToken) {
-      setIsLoggedIn(true);
-      setUserToken(storedUserToken);
+    // Confirm with the user before logging out
+    if (window.confirm("Are you sure you want to log out?")) {
+      // Remove the userToken cookie
+      Cookies.remove("userToken");
+      // Update the isLoggedIn state
+      setIsLoggedIn(false);
     }
-  }, []);
+  };
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-6 md:space-x-10">
-          {/* Logo on the far left */}
           <div className="flex-shrink-0">
             <Link href="/">
-              <img src="/melodyscout.png" className="h-8 w-auto" alt="Logo" />
+              <a>
+                <img src="/melodyscout.png" className="h-8 w-auto" alt="Logo" />
+              </a>
             </Link>
           </div>
-
-          {/* Log in or Log out button on the far right */}
           <div className="flex-shrink-0">
             {isLoggedIn ? (
               <button onClick={handleLogoutClick}>Log Out</button>
             ) : (
-              <a href="#" onClick={handleLoginClick}>
-                Log in
-              </a>
+              <button onClick={handleLoginClick}>Log In</button>
             )}
           </div>
         </div>
